8490f8ca3c32c399f01d1056c44e5d1475a2e9fb
Roman <simenk@mail.ru>
2021-04-07 08:25:39 +0300

    eslint init, make lint set

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,8 @@
 import readlineSync from 'readline-sync';
 
-
-const userName = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    return console.log('Hello ' + userName + '!');
+const userNameAlert = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  return console.log(`Hello ${userName} !`);
 };
 
-export { userName };
+export default userNameAlert;
