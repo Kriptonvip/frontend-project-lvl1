cf87d8c2813484da804288345494b125f1bd0d5b
Kripton <simenk@mail.ru>
2021-04-09 22:42:46 +0300

    change for test2

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,6 @@ import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games');
 const userName = readlineSync.question('May I have your name? ');
-const userNameAlert = () => console.log(`Hello ${userName}!`);
+const userNameAlert = () => console.log(`Hello, ${userName}!`);
 
 export { userNameAlert, userName };
