26a281fabb03b7f334ae915eb40a714b7da75be6
Kripton <simenk@mail.ru>
2022-01-28 01:34:45 +0300

    Refactoring 3

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 const userName = () => readlineSync.question('May I have your name? ');
 
 const userNameAlert = () => {
-  console.log('Welcome to the Brain Games');
+  console.log('Welcome to the Brain Games!');
   console.log(`Hello, ${userName()}!`);
 };
 
